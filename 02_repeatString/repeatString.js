0864385dea733de28db53e218a77a5dff6ddbdfd
Julian Khan <juliankhan.b@gmail.com>
2022-08-05 12:41:27 +0800

    Complete JavaScript exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word, repeatNum) {
+    let multipliedString = '';
 
+    if (repeatNum < 0) {
+        return 'ERROR';
+    }
+
+    for (i = 0; i < repeatNum; i++) {
+        multipliedString += word;
+    }
+
+    return multipliedString;
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
