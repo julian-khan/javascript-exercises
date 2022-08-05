efed03b6b22baadf27744ea0772b2b7690d0fb03
Julian Khan <juliankhan.b@gmail.com>
2022-08-05 20:34:57 +0800

    Complete Exercise 3 and Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeElements) {
+    arrCopy = [...arr];
 
+    for (const removeEl of removeElements) {
+        const index = arrCopy.indexOf(removeEl);
+
+        if (index !== -1) {
+            arrCopy.splice(index, 1);
+        }
+    }
+
+    return arrCopy;
 };
 
 // Do not edit below this line
