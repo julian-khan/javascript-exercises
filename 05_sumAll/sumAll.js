4dc3fcdbf8322aa8e0fe1a7e2c0aca85a0460593
Julian Khan <juliankhan.b@gmail.com>
2022-08-05 20:57:41 +0800

    Complete Exercise 5 and Exercise 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    
+    if (!(Number.isInteger(int1) && Number.isInteger(int2))) {
+        return 'ERROR';
+    } else if (int1 < 0 || int2 < 0) {
+        return 'ERROR';
+    }
 
+    summedNumbers = 0;
+    for (i = Math.min(int1, int2); i <= Math.max(int1, int2); i++) {
+        summedNumbers += i;
+    }
+
+    return summedNumbers
 };
 
 // Do not edit below this line
