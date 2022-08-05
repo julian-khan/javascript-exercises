7b3c9203ab475e30dfef04f4ff6c37c49a4a9da3
Julian Khan <juliankhan.b@gmail.com>
2022-08-05 20:35:28 +0800

    Complete Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    reversedArray = string.split('').reverse();
+    return reversedArray.join('');
 };
 
 // Do not edit below this line
