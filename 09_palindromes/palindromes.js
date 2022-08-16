9a10011cd1034a597e74c26648e43a9af3d07397
Julian Khan <juliankhan.b@gmail.com>
2022-08-16 19:27:08 +0800

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    
+    const stringFormatted = word.toLowerCase().replace(/\W/g, '');
+    const forwardArr = stringFormatted.split('');
 
-};
+    const reversedArr = [...forwardArr].reverse();
+
+    for (i = 0; i < forwardArr.length -1; i++) {
+        if (forwardArr[i] !== reversedArr[i]) {
+            console.log(false);
+            return false
+        }
+    }
+    return true
+}
+    
+
 
 // Do not edit below this line
 module.exports = palindromes;
+
