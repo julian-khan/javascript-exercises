e8971de9a7c94507b86110c3a2f7e098ad3b03a7
Julian Khan <juliankhan.b@gmail.com>
2022-08-17 15:05:33 +0800

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(finalIndex) {
+    if (finalIndex < 0) {
+        return 'OOPS';
+    }
 
+    let arr = [0, 1];
+
+    if (finalIndex == 0) {
+        return arr[0]
+    } else if (finalIndex == 1) {
+        return arr[1]
+    }
+
+    if (finalIndex >= 2) {
+
+    for (i = 2; i <= finalIndex; i++) {
+        arr.push(arr[i-1] + arr[i-2]);
+    }
+    return arr.pop();
+    }
 };
 
 // Do not edit below this line
