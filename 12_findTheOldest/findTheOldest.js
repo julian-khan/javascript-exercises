1402e2dd792cd371127db3756d0b0477d06ce22d
Julian Khan <juliankhan.b@gmail.com>
2022-08-18 20:51:27 +0800

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,47 +1,27 @@
 const findTheOldest = function(people) {
 
-  people.forEach((person) => { //extract to separate function
-    if (!( person.hasOwnProperty('yearOfDeath'))) {
-
-    const date = new Date();
-    const currentYear = date.getFullYear();
-
-    person.age = currentYear - person.yearOfBirth;
-    } else { 
-    person.age = person.yearOfDeath - person.yearOfBirth;
-    }
-  });
+  calculateAge(people);
 
   people.sort((a, b) => { 
     return a.age - b.age;
   });
-
-  return people[-1]; //FIX this return statement
-
+  return people.pop();
 };
 
+function calculateAge(people) {
+  people.forEach((person) => {
+    if (!(person.hasOwnProperty('yearOfDeath'))) {
 
-const people = [
-          {
-        name: "Carly",
-        yearOfBirth: 1942,
-        yearOfDeath: 1970,
-      },
-      {
-        name: "Ray",
-        yearOfBirth: 1962,
-        yearOfDeath: 2011,
-      },
-      {
-        name: "Jane",
-        yearOfBirth: 1912,
-        yearOfDeath: 1941,
-      },
-  ]
+      const date = new Date();
+      const currentYear = date.getFullYear();
 
-  
-
-findTheOldest(people);
+      person.age = currentYear - person.yearOfBirth;
+    } else {
+      person.age = person.yearOfDeath - person.yearOfBirth;
+    }
+  });
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
