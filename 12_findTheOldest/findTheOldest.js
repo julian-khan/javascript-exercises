52a85b5bd9d87eca1dbf49c489e82f8978d308ff
Julian Khan <juliankhan.b@gmail.com>
2022-08-18 12:58:57 +0800

    Create functions for findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,47 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+  people.forEach((person) => { //extract to separate function
+    if (!( person.hasOwnProperty('yearOfDeath'))) {
+
+    const date = new Date();
+    const currentYear = date.getFullYear();
+
+    person.age = currentYear - person.yearOfBirth;
+    } else { 
+    person.age = person.yearOfDeath - person.yearOfBirth;
+    }
+  });
+
+  people.sort((a, b) => { 
+    return a.age - b.age;
+  });
+
+  return people[-1]; //FIX this return statement
 
 };
+
+
+const people = [
+          {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+  ]
+
+  
+
+findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
